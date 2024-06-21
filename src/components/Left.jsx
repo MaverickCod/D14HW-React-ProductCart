37f16a251c72cedc76cfed5d06336e9a6f7590db
MavericCod <921chavanprem@gmail.com>
2024-06-21 22:19:40 +0530

    made some changes to resolve errors

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -1,77 +1,72 @@
-import { useContext, useEffect } from "react"
-import { productContext } from "../App"
-
-import Product from "./Product"
+import { useContext, useEffect } from "react";
+import { productContext } from "../App";
+import Product from "./Product";
 
 function Left() {
+  const { products, setProducts, total, setTotal, cart, setCart } = useContext(productContext);
 
-  const { products , setProducts , total , setTotal , cart , setCart} = useContext(productContext)
-
-  
-  useEffect(()=>{
+  useEffect(() => {
     let totalAmount = 0;
-    if(cart.length > 0){
-    cart.forEach((item,index)=>{
-     totalAmount += Number(item.price) * Number(item.quantity);
-    })
-    setTotal(totalAmount);
-  }
-   },[cart])
-
-   useEffect(()=>{
-       let copyArray = [...products];
-       setCart(copyArray.filter((item,index)=>{
-        return item.quantity > 0;
-       }))
-   },[products])
-
-
-  function handleCartRemove(e, id){
-    e.preventDefault()
-    setProducts(products.filter((prdItem)=>{
-       return prdItem.id !== id;
-    }))
-  }
-
-  function handleDecrement(index){
-
-    let copyCart = [...products];
-    
-     let count = copyCart[index]["quantity"] 
-  
-     if(count > 0){
-      copyCart[index]["quantity"] -= 1;
-     }
-     setProducts(copyCart)
-
-     if(count <= 0){
-      setProducts(products.filter((cartItem,idx)=>{
-        return index !== idx;
-      }))
+    if (cart.length > 0) {
+      cart.forEach((item) => {
+        totalAmount += Number(item.price) * Number(item.quantity);
+      });
+      setTotal(totalAmount);
     }
-}
+  }, [cart, setTotal]);
 
-function handleIncrement(index){
-  let copyCart = [...products];
-  copyCart[index]["quantity"] += 1;
-  setProducts(copyCart)
+  useEffect(() => {
+    const copyArray = [...products];
+    setCart(copyArray.filter((item) => item.quantity > 0));
+  }, [products, setCart]);
 
-}
+  function handleCartRemove(e, id) {
+    e.preventDefault();
+    setProducts(products.filter((prdItem) => prdItem.id !== id));
+  }
+
+  function handleDecrement(index) {
+    const copyProducts = [...products];
+    let count = copyProducts[index].quantity;
+
+    if (count > 0) {
+      copyProducts[index].quantity -= 1;
+      setProducts(copyProducts);
+
+      if (count <= 1) {
+        setProducts(products.filter((_, idx) => index !== idx));
+      }
+    }
+  }
+
+  function handleIncrement(index) {
+    const copyProducts = [...products];
+    copyProducts[index].quantity += 1;
+    setProducts(copyProducts);
+  }
 
   return (
-   <>
-     <div className="left">
-       <h2>Product</h2>
-
-       {products.length > 0 ?  products.map((dt,idx)=>{
-        return <Product key={idx} image={dt.image} phoneName={dt.phoneName} price={dt.price} id={dt.id} quantity={dt.quantity} 
-        handleCartRemove={handleCartRemove} handleDecrement={handleDecrement} handleIncrement={handleIncrement} index={idx}/>
-     })   
-       : <div className="refresh">No Product is their !! kindly Refresh </div>}
-      
-     </div>
-   </>
-  )
+    <div className="left">
+      <h2>Product</h2>
+      {products.length > 0 ? (
+        products.map((dt) => (
+          <Product
+            key={dt.id}
+            image={dt.image}
+            phoneName={dt.phoneName}
+            price={dt.price}
+            id={dt.id}
+            quantity={dt.quantity}
+            handleCartRemove={handleCartRemove}
+            handleDecrement={() => handleDecrement(dt.id)}
+            handleIncrement={() => handleIncrement(dt.id)}
+          />
+        ))
+      ) : (
+        <div className="refresh">No Product is there!! Kindly Refresh</div>
+      )}
+    </div>
+  );
 }
 
-export default Left
+export default Left;
